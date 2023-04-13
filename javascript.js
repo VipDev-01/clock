ceef5cb86717e217ec6fdebd4876de2e908c16b0
VipDev-01 <vipul713@gmail.com>
2023-04-13 16:07:02 +0400

    styled

---
+++ javascript.js
@@ -0,0 +1,29 @@
+setInterval(setDate,1000);
+
+const secHand = document.querySelector('.h1');
+const minsHand = document.querySelector('.h2');
+const hoursHand = document.querySelector('.h3');
+const hands = document.querySelector('.hand');
+
+
+
+function setDate() {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const secondsDeg = ((seconds / 60) * 360 + 90);
+    
+    if (secondsDeg > 432) {hands.style.setProperty('transition', 'null')} else {hands.style.setProperty('transition', '')}
+
+    const mins = date.getMinutes();
+    const minsDeg = ((mins / 60) * 360 + 90);
+
+    const hours = date.getHours();
+    const hoursDeg = ((hours / 12) * 360 + 90);
+
+
+    secHand.style.setProperty('rotate', secondsDeg + 'deg');
+    minsHand.style.setProperty('rotate', minsDeg + 'deg');
+    hoursHand.style.setProperty('rotate', hoursDeg + 'deg');
+
+}
+
